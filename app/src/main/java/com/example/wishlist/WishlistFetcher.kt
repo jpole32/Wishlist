d4a82bd9ce5f437498d8d080384e225099be7d8c
jpole329 <jpoleary329@gmail.com>
2024-02-05 11:41:13 -0500

    Finished basic listing / no user input yet

--- app/src/main/java/com/example/wishlist/WishlistFetcher.kt
+++ app/src/main/java/com/example/wishlist/WishlistFetcher.kt
@@ -2,25 +2,20 @@ package com.example.wishlist
 
 class WishlistFetcher {
     companion object {
-        val items = listOf("Some Item")
-        val prices = listOf("1.99")
-        val urls = listOf("www.idk.com")
+        val items = listOf("Some Item","Second item")
+        val prices = listOf("1.99","2.00")
+        val urls = listOf("www.idk.com","somesite.com")
         fun getWishes(): MutableList<Wishlist> {
-            var wishes : MutableList<Wishlist> = ArrayList()
-            for (i in 0..9) {
-                val wishitem = Wishlist(items[i], prices[i], urls[i])
-                wishes.add(wishitem)
+            var wishes: MutableList<Wishlist> = ArrayList()
+            var listLimit = items.size
+            if (listLimit > 0){
+                for (i in 0..(listLimit-1)) {
+                    val wishitem = Wishlist(items[i], prices[i], urls[i])
+                    wishes.add(wishitem)
+                }
             }
             return wishes
         }
 
-        fun getNext5Wishes(): MutableList<Wishlist> {
-            var newWishes : MutableList<Wishlist> = ArrayList()
-            for (i in 10..14) {
-                val wish = Wishlist(items[i], prices[i], urls[i])
-                newWishes.add(wish)
-            }
-            return newWishes
-        }
     }
 }
