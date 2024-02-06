16f526702120413fab926416210c14553bcf6509
jpole329 <jpoleary329@gmail.com>
2024-02-05 23:39:03 -0500

    Items added based on user input

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.wishlist
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         lateinit var wishes: List<Wishlist>
         super.onCreate(savedInstanceState)
@@ -13,6 +17,13 @@ class MainActivity : AppCompatActivity() {
 
         // Lookup the RecyclerView in activity layout
         val wishRv = findViewById<RecyclerView>(R.id.wishlistRv)
+        val button = findViewById<Button>(R.id.addTo)
+        val itemToAdd = findViewById<EditText>(R.id.itemTitle)
+        val priceToAdd = findViewById<EditText>(R.id.itemPrice)
+        val linkToAdd = findViewById<EditText>(R.id.itemLink)
+        var textTitle = ""
+        var textPrice = ""
+        var textLink = ""
         // Fetch the list of emails
         wishes = WishlistFetcher.getWishes()
         // Create adapter passing in the list of emails
@@ -21,5 +32,14 @@ class MainActivity : AppCompatActivity() {
         wishRv.adapter = adapter
         // Set layout manager to position the items
         wishRv.layoutManager = LinearLayoutManager(this)
+        button.setOnClickListener{
+            textTitle = itemToAdd.text.toString()
+            textPrice = priceToAdd.text.toString()
+            textLink = linkToAdd.text.toString()
+            val nWishes = WishlistFetcher.addItem(textTitle,textPrice,textLink)
+            wishes.addAll(nWishes)
+            // Attach the adapter to the RecyclerView to populate items
+            adapter.notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/example/wishlist/WishlistFetcher.kt
+++ app/src/main/java/com/example/wishlist/WishlistFetcher.kt
@@ -1,21 +1,36 @@
 package com.example.wishlist
 
+import android.util.Log
+
 class WishlistFetcher {
     companion object {
-        val items = listOf("Some Item","Second item")
-        val prices = listOf("1.99","2.00")
-        val urls = listOf("www.idk.com","somesite.com")
+        val items = mutableListOf<String>()
+        val prices = mutableListOf<String>()
+        val urls = mutableListOf<String>()
         fun getWishes(): MutableList<Wishlist> {
             var wishes: MutableList<Wishlist> = ArrayList()
-            var listLimit = items.size
+            /*var listLimit = items.size
+            Log.d("Wishlist", "inside wishes, current length of list is "+listLimit.toString())
             if (listLimit > 0){
                 for (i in 0..(listLimit-1)) {
                     val wishitem = Wishlist(items[i], prices[i], urls[i])
+                    Log.d("WishITem", items[i]+prices[i]+urls[i])
                     wishes.add(wishitem)
                 }
-            }
+            }*/
             return wishes
+        }
+        fun addItem(iName: String, iPrice: String, iLink: String): MutableList<Wishlist> {
+            var newWish : MutableList<Wishlist> = ArrayList()
+            val wish = Wishlist(iName, iPrice, iLink)
+            Log.d("Wishlist", "items added to wishlist, calling getwishes")
+            newWish.add(wish)
+            return newWish
+
         }
 
     }
+
+
+
 }
